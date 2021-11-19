7cd073b0d8428a85966e7877d74ce25c299dbd61
gleibento <glei-bento@hotmail.com>
2021-11-18 22:53:32 -0300

    created backend project

---
+++ backend/src/test/java/com/gbdsolution/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbdsolution.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
